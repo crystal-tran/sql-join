ab1209be252e31f722bbd0d20e1493872dc856cb
Crystal Tran <c.t.tran95@gmail.com>
2023-12-05 15:16:39 -0500

    completed query to join to tables

--- queries.sql
+++ queries.sql
@@ -1,1 +1,25 @@
 -- write your queries here
+
+SELECT *
+FROM owners
+    LEFT JOIN vehicles
+        ON owners.id = vehicles.owner_id
+ORDER BY owners.id,
+    vehicles.id;
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- Personal notes
+-- We used left join because we see that owner_id in the table vehicles is a foreign key, and is not nullable
+-- therefore we know it will have a owner_id assigned to each row
+-- Don't necessarily need to use full but it will work for this case.
